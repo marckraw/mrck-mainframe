bfd7fc6bad465bc17b8e7629b62b78b841c73a95
Marcin Krawczyk <marckraw@icloud.com>
2020-11-29 20:22:53 +0100

    [upd] some packages and project name

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from './layout.module.scss'
 import utilStyles from '../styles/utils.module.scss'
 
-const name = 'Marcin Krawczyk'
+const name = 'mrck'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({children, home}) {
