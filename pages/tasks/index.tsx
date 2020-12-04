9e193cb33e21dc0c4451ecc451d6b00d447715b0
Marcin Krawczyk <marckraw@icloud.com>
2020-12-04 11:50:04 +0100

    [add] hardcode tags

--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -1,13 +1,13 @@
 import {GetStaticProps} from 'next'
 import Head from 'next/head'
 import Layout from '../../components/layout'
+import Label from '../../components/Label'
 
 interface TasksIndexProps {
     dupa: string
 }
 
 export default function TasksIndex(props: TasksIndexProps): JSX.Element {
-    console.log(props)
     return (
         <Layout home>
             <Head>
@@ -16,15 +16,15 @@ export default function TasksIndex(props: TasksIndexProps): JSX.Element {
             <section>
                 <h2>This is tasks section</h2>
                 <ul>
-                    <li>Fix persistence of data in Virtual Office <span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
-                    <li>GUI for sb-mig <span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
-                    <li>Build task manager in mrck-mainframe <span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
-                    <li>Zorganizować prezenty dla:
+                    <li><Label name="EF" /> <Label name="Virtual Office" /> [ ] Fix persistence of data in Virtual Office <span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                    <li><Label name="EF" /> <Label name="sb-mig" type="private" /> [ ] GUI for sb-mig <span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                    <li><Label name="mrck-mainframe" type="private" /> [ ] Build task manager in mrck-mainframe <span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                    <li>[ ] Zorganizować prezenty dla:
                         <ol>
-                            <li>Kasiu i Bena<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
-                            <li>Mamy i Taty<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
-                            <li>Gosi<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
-                            <li>Babci Gieni?<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                            <li><Label name="Prezenty" type="private" /> Kasiu i Bena<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                            <li><Label name="Prezenty" type="private" /> Mamy i Taty<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                            <li><Label name="Prezenty" type="private" /> Gosi<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
+                            <li><Label name="Prezenty" type="private" /> Babci Gieni?<span style={{color: 'orange', fontSize: '10px'}}>{`<show task details>`}</span></li>
                         </ol>
                     </li>
                 </ul>
